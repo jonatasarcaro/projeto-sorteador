a23ad69cff66f8b269de7fa8e375f9c221bdf302
jonatasarcaro <jontasarcaro943@gmail.com>
2024-09-08 21:45:50 -0300

    começo do projeto javascript

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,4 @@
+function sortear (){
+  let quantidade = document.getElementById('quantidade').value; // criado uma função "sortear" para e declara uma variavel e puxa seu id do input quantidade
+
+}
